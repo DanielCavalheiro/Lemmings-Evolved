191c70334982c4edb2e602ce5613757e593fa837
Daniel <d.cavalheiro@fct.unl.pt>
2022-03-31 13:12:16 +0100

    falta fazer os trials

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+
+    //se calhar isto tem de ser numa class à parte mesmo
+    public record Lemming(char tribe, int power){};
+    
+    private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numOfTrials=Integer.parseInt(in.readLine());
+        //Array where we keep all game trials to later print
+        Trial[] trials=new Trial[numOfTrials]; 
+
+        //read input one trial at a time and plays it
+        for(int trialNum=0; trialNum<numOfTrials; trialNum++){
+            int numOfLemmingsInLine=Integer.parseInt(in.readLine());
+            Lemming[] line1 = processLine(numOfLemmingsInLine);
+
+            numOfLemmingsInLine=Integer.parseInt(in.readLine());
+            Lemming[] line2 =processLine(numOfLemmingsInLine);
+
+            trials[trialNum]=new Trial(line1, line2);
+        }
+        in.close();
+
+        for
+
+    }
+
+    private static Lemming[] processLine(int numOfLemmings) throws IOException {
+        Lemming[] line=new Lemming[numOfLemmings];
+
+        for (int i = 0; i < numOfLemmings; i++) {
+            String[] tokens=in.readLine().split(" ");
+            char tribe=tokens[0].toCharArray()[0];
+            int power=Integer.parseInt(tokens[1]);
+            line[i]=new Lemming(tribe, power);
+        }
+           
+        return line;
     }
 }
---
+++ src/Trial.java
@@ -0,0 +1,27 @@
+
+public class Trial {
+
+    int maxpoints;
+    int minPairs;
+    
+    public Trial(Lemming[] line1, Lemming[] line2){
+
+        //Processar cada linha numa matriz 
+        /**
+         * -começa se por encher a primeira linha e primeira coluna
+         * -depois pecorremos a matriz linha a linha
+         * -cada entrada séra igual ao maximo entre a entrada à sua esquerda, a entrada em cima , a entrada na sua diagonal esquerda (para cima) 
+         *     mais a soma do power dos lemmings se foram da mesmo tribo
+         * -assim quando chegarmos ao final da tabela a ultima entrada será a pontuação maxima
+         * -acho eu neh para dizer a verdade fazia bue sentido quando estava a testar em papel mas agora que estou a escrever isto parece que nao é bem assim
+         * lá está né 
+         */
+
+    }
+
+    @Override
+    public String toString(){
+        return //"maxPoints minPairs";
+    }
+    
+}
