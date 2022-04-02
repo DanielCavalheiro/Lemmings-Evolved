585741abb2cf120aae75fc45424155436fe6151f
PedroN2001 <pj.nunes@campus.fct.unl.pt>
2022-04-02 20:00:08 +0100

    New changes bruh

--- src/Lemming.java
+++ src/Lemming.java
@@ -1,3 +1,21 @@
-public record Lemming(char tribe, int power) {
-    
+public class Lemming {
+
+    private char tribe;
+    private int power;
+
+    public Lemming(char tribe, int power) {
+
+        this.tribe = tribe;
+        this.power = power;
+
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public char getTribe() {
+        return tribe;
+    }
+
 }
--- src/Trial.java
+++ src/Trial.java
@@ -1,20 +1,13 @@
 
 public class Trial {
 
-    private int maxPoints;
-    private int minPairs;
+    private long maxPoints;
+    private long minPairs;
 
     public Trial(Lemming[] line1, Lemming[] line2, int numOfLemmingsInLine1, int numOfLemmingsInLine2) {
 
-        // a minha ideia para minPairs é fazer uma nova tabela com o mesmo tamanho e
-        // guardar o numero de pares em cada celula e heredita da mesma forma
-        // quando chegas a um novo par fazer +1, agora obviamente que heredita o power
-        // maior mas se o power for igual basta escolher o numero de para mais pequeno
-        // Cumprimentos
-        // Lá está né
-
-        int[][] score = new int[numOfLemmingsInLine1 + 1][numOfLemmingsInLine2 + 1];
-        int[][] pairs = new int[numOfLemmingsInLine1 + 1][numOfLemmingsInLine2 + 1];
+        long[][] score = new long[numOfLemmingsInLine1 + 1][numOfLemmingsInLine2 + 1];
+        long[][] pairs = new long[numOfLemmingsInLine1 + 1][numOfLemmingsInLine2 + 1];
 
         for (int i = 0; i <= numOfLemmingsInLine1; i++) { // Column 0: base case, line1=()
             score[i][0] = 0;
@@ -26,7 +19,7 @@ public class Trial {
             pairs[0][j] = 0;
         }
 
-        int leftScore, upScore, leftPairs, upPairs;
+        long leftScore, upScore, leftPairs, upPairs, bestScore;
 
         for (int j = 1; j <= numOfLemmingsInLine2; j++) {
 
@@ -38,18 +31,24 @@ public class Trial {
                 leftPairs = pairs[i - 1][j];
                 upPairs = pairs[i][j - 1];
 
-                score[i][j] = Math.max(leftScore, upScore);
-
-                if(leftScore == upScore)
-                    pairs[i][j] = Math.min(leftPairs, upPairs);
-                else if(leftScore > upScore)
-                    pairs[i][j] = leftPairs;
-                else
-                    pairs[i][j] = upPairs;
-
-                if (line1[i - 1].tribe() == line2[j - 1].tribe()) {
-                    score[i][j] += (line1[i - 1].power() + line2[j - 1].power());
+                if (line1[i - 1].getTribe() == line2[j - 1].getTribe()) {
+                    score[i][j] = (line1[i - 1].getPower() + line2[j - 1].getPower());
                     pairs[i][j]++;
+                }
+
+                score[i][j] += score[i - 1][j - 1];
+                pairs[i][j] += pairs[i - 1][j - 1];
+
+                bestScore = Math.max(leftScore, upScore);
+
+                if (score[i][j] < bestScore) {
+
+                    score[i][j] = bestScore;
+
+                    if (bestScore == leftScore)
+                        pairs[i][j] = leftPairs;
+                    else
+                        pairs[i][j] = upPairs;
                 }
             }
 
@@ -57,20 +56,6 @@ public class Trial {
 
         maxPoints = score[numOfLemmingsInLine1][numOfLemmingsInLine2];
         minPairs = pairs[numOfLemmingsInLine1][numOfLemmingsInLine2];
-
-        // Processar cada linha numa matriz
-        /**
-         * -começa se por encher a primeira linha e primeira coluna
-         * -depois pecorremos a matriz linha a linha
-         * -cada entrada séra igual ao maximo entre a entrada à sua esquerda, a entrada
-         * em cima , a entrada na sua diagonal esquerda (para cima)
-         * mais a soma do power dos score se foram da mesmo tribo
-         * -assim quando chegarmos ao final da tabela a ultima entrada será a pontuação
-         * maxima
-         * -acho eu neh para dizer a verdade fazia bue sentido quando estava a testar em
-         * papel mas agora que estou a escrever isto parece que nao é bem assim
-         * lá está né
-         */
 
     }
 
